6a85cd11391587602c1783f59193801b4fab2a9b
swarnaa <swarna.annamaneni@techmahindra.com>
2016-02-22 17:29:26 +0530

    Second commit from swarna

--- src/Sample.java
+++ src/Sample.java
@@ -4,7 +4,10 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	System.out.println("Hi Iam in Tech M");	
+	  System.out.println("Hi Iam in Tech M");
+	  System.out.println("Hi Iam in Tech M 1111111111");
+	  System.out.println("Hi Iam in Tech M 22222222222");
+	  
 	}
 
 }
